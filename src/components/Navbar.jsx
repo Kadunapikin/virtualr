8b906ee7b38acfe331b8ea69e2199e7fdf0337c4
Kadunapikin <ibakoshi27@gmail.com>
2024-09-18 12:21:39 +0100

    make Navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
+import { MenuIcon, X } from 'lucide-react';
+import { useState } from 'react';
 import logo from '../assets/logo.png';
 import { navItems } from '../constants';
+
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    
+    const handleToggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
         <div className="container px-4 mx-auto relative tx-sm">
@@ -24,7 +32,31 @@ const Navbar = () => {
                         Create an account
                     </a>
                 </div>
+                <div className="lg:hidden md:flex flex-col justify-end">
+                    <button onClick={handleToggleMenu}>
+                        {isMenuOpen ? <X /> : <MenuIcon />}
+                    </button>
+                </div>
             </div>
+            {isMenuOpen && (
+                <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+                    <ul>
+                        {navItems.map((item, index) => (
+                            <li key={index} className='py-4'>
+                                <a href={item.href}>{item.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="flex space-x-6">
+                        <a href="#" className='py-2 px-3 border rounded-md'>
+                            Sign in
+                        </a>
+                        <a href="#" className='py-2 px-3 border rounded-md bg-gradient-to-r from-orange-500 to-orange-800'>
+                            Create an account
+                        </a>
+                    </div>
+                </div>
+            )}
         </div>
     </nav>
   )
